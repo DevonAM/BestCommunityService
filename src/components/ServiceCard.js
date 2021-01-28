45808997a000e775a1746ceced0e9b0ce1fc11e2
Devon Acree-Meza <dacreeme@asu.edu>
2021-01-28 13:50:20 -0700

    Create Services page and card

---
+++ src/components/ServiceCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ServicesPage from "./ServicesPage";
+
+const ServiceCard = ({ name, description }) => {
+  return (
+    <div className="card">
+      <div className="image">
+        <img src="https://www.delfield.com/getattachment/5ab7c000-f6bc-4602-a805-b48a78924ecd/image.jpg.aspx?sitename=Delfield" />
+      </div>
+
+      <div className="content">
+        <div className="header">{name}</div>
+        <div className="description">{description}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ServiceCard;
