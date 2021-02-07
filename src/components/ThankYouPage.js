874d32050047b313c8a43b6dd1013b4e607cec12
Devon Acree-Meza <dacreeme@asu.edu>
2021-02-07 15:31:52 -0700

    add account and thankyou page

---
+++ src/components/ThankYouPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class ThankYouPage extends React.Component {
+  render() {
+    return (
+      <div className="ui center aligned grid">
+        <h1>Thank You!</h1>
+        <img alt="thankyou" src="./logo512.png" />
+      </div>
+    );
+  }
+}
+
+export default ThankYouPage;
