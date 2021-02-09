4851355bec77e9f00c73ce0d2297231b288d1bea
Devon Acree-Meza <dacreeme@asu.edu>
2021-02-09 11:05:09 -0700

    Start conditional rendering menubar

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 // const logo = "./logo512.png";
 
-const renderContent = () => {
+const renderMenu = () => {
   return (
     <div className="pusher" style={{ marginBottom: "4em" }}>
       <div className="ui inverted vertical masthead center aligned segment">
@@ -36,10 +36,44 @@ const renderContent = () => {
   );
 };
 
+const renderLoggedMenu = () => {
+  return (
+    <div className="pusher" style={{ marginBottom: "4em" }}>
+      <div className="ui inverted vertical masthead center aligned segment">
+        <div className="ui container">
+          <div className="ui inverted borderless top fixed fluid menu">
+            {/* <a className="toc item">
+              <i className="sidebar icon"></i>
+            </a> */}
+            <Link to="/" className="active item">
+              Home
+            </Link>
+            <Link to="/services" className="item">
+              Services
+            </Link>
+            <Link to="/donate" className="item">
+              Donate
+            </Link>
+            <div className="right item">
+              <Link to="/account" className="ui inverted button">
+                Account
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 class MenuBar extends React.Component {
   state = { activePage: null };
+
   render() {
-    return renderContent();
+    if (this.props.isLoggedIn) {
+      return renderLoggedMenu();
+    }
+    return renderMenu();
   }
 }
 
