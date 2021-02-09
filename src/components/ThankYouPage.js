8498522ece2b2d7f5653dbe587c20aba9f4a4c81
Devon Acree-Meza <dacreeme@asu.edu>
2021-02-09 11:04:56 -0700

    update thankyou page

--- src/components/ThankYouPage.js
+++ src/components/ThankYouPage.js
@@ -4,8 +4,17 @@ class ThankYouPage extends React.Component {
   render() {
     return (
       <div className="ui center aligned grid">
-        <h1>Thank You!</h1>
-        <img alt="thankyou" src="./logo512.png" />
+        <div className="ui segment " style={{ marginBottom: "2em" }}>
+          <h1 className="ui center aligned icon header">
+            <i className="circular shopping bag icon" />
+            Thank You!
+          </h1>
+          <h3>
+            You are registered for Art Class on 3/21/2021 at 12:30pm with Devon
+            Acree-Meza
+          </h3>
+          <h4>Our address is: 1234 W. Street Ln. 86730 Aspen, CO</h4>
+        </div>
       </div>
     );
   }
