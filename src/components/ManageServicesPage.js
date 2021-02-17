930b935d52749fa854827c57ff2f96ad56aec9c7
Devon Acree-Meza <dacreeme@asu.edu>
2021-02-17 16:40:03 -0700

    move modal to component

--- src/components/ManageServicesPage.js
+++ src/components/ManageServicesPage.js
@@ -1,49 +1,19 @@
 import React from "react";
 import SideBar from "./SideBar";
+import Modal from "./Modal";
 
 class ManageServicesPage extends React.Component {
   constructor() {
     super();
     this.state = { showModal: true };
-    this.renderModal = this.renderModal.bind(this);
+    // this.RenderModal = this.RenderModal.bind(this);
   }
-  renderModal = () => {
-    <div class="ui modal">
-      <i class="close icon"></i>
-      <div class="header">Profile Picture</div>
-      <div class="image content">
-        <div class="ui medium image">
-          <img src="/images/avatar/large/chris.jpg" />
-        </div>
-        <div class="description">
-          <div class="ui header">
-            We've auto-chosen a profile image for you.
-          </div>
-          <p>
-            We've grabbed the following image from the{" "}
-            <a href="https://www.gravatar.com" target="_blank">
-              gravatar
-            </a>{" "}
-            image associated with your registered e-mail address.
-          </p>
-          <p>Is it okay to use this photo?</p>
-        </div>
-      </div>
-      <div class="actions">
-        <div class="ui black deny button">Nope</div>
-        <div class="ui positive right labeled icon button">
-          Yep, that's me
-          <i class="checkmark icon"></i>
-        </div>
-      </div>
-    </div>;
-  };
 
   render() {
     return (
       <div>
-        {this.state.showModal ? this.renderModal() : null}
-        <div className="ui mobile only padded grid"></div>
+        <Modal />
+        {/* <div className="ui mobile only padded grid"></div>
         <div className="ui padded grid">
           <SideBar activePage="services" />
           <div
@@ -156,7 +126,7 @@ class ManageServicesPage extends React.Component {
               </div>
             </div>
           </div>
-        </div>
+        </div>*/}
       </div>
     );
   }
