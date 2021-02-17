c2ba4e8e760d7a8a6d728fdeb12ac5fc5a4b81cf
Devon Acree-Meza <dacreeme@asu.edu>
2021-02-17 14:55:11 -0700

    add modal

---
+++ src/components/Modal.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Modal = () => {
+  return (
+    <div class="ui modal">
+      <div class="header">Header</div>
+      <div class="content">
+        <p>sdf</p>
+        <p>sasdfa</p>
+        <p>asdfasdfasdf</p>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
