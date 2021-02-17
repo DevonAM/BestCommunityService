f3f08ff21e39e95ff534546bc7bc489d91815311
Devon Acree-Meza <dacreeme@asu.edu>
2021-02-17 15:25:24 -0700

    .

--- src/components/ManageServicesPage.js
+++ src/components/ManageServicesPage.js
@@ -2,9 +2,47 @@ import React from "react";
 import SideBar from "./SideBar";
 
 class ManageServicesPage extends React.Component {
+  constructor() {
+    super();
+    this.state = { showModal: true };
+    this.renderModal = this.renderModal.bind(this);
+  }
+  renderModal = () => {
+    <div class="ui modal">
+      <i class="close icon"></i>
+      <div class="header">Profile Picture</div>
+      <div class="image content">
+        <div class="ui medium image">
+          <img src="/images/avatar/large/chris.jpg" />
+        </div>
+        <div class="description">
+          <div class="ui header">
+            We've auto-chosen a profile image for you.
+          </div>
+          <p>
+            We've grabbed the following image from the{" "}
+            <a href="https://www.gravatar.com" target="_blank">
+              gravatar
+            </a>{" "}
+            image associated with your registered e-mail address.
+          </p>
+          <p>Is it okay to use this photo?</p>
+        </div>
+      </div>
+      <div class="actions">
+        <div class="ui black deny button">Nope</div>
+        <div class="ui positive right labeled icon button">
+          Yep, that's me
+          <i class="checkmark icon"></i>
+        </div>
+      </div>
+    </div>;
+  };
+
   render() {
     return (
       <div>
+        {this.state.showModal ? this.renderModal() : null}
         <div className="ui mobile only padded grid"></div>
         <div className="ui padded grid">
           <SideBar activePage="services" />
@@ -37,7 +75,10 @@ class ManageServicesPage extends React.Component {
                       <td>$100</td>
                       <td>sit</td>
                       <td>
-                        <i className="ui icon edit" />
+                        <i
+                          className="ui icon edit"
+                          // onClick={this.setState({ showModal: true })}
+                        />
                       </td>
                     </tr>
                     <tr>
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-const Modal = () => {
-  return (
-    <div class="ui modal">
-      <div class="header">Header</div>
-      <div class="content">
-        <p>sdf</p>
-        <p>sasdfa</p>
-        <p>asdfasdfasdf</p>
-      </div>
-    </div>
-  );
-};
+class Modal extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // this.renderModal = this.renderModal.bind(this);
+  }
+
+  render() {
+    // return this.renderModal();
+    return <div>hello</div>;
+  }
+}
 
 export default Modal;
